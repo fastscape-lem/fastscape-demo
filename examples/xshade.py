105c8507faf6eb304929171dd56bc0d392b329c9
Benoit Bovy <benbovy@gmail.com>
2020-01-30 10:09:50 +0100

    fix compat with datashader 0.9.0+

--- examples/xshade.py
+++ examples/xshade.py
@@ -5,11 +5,16 @@ def hillshade(ds, groupby=None, elev_var='topography__elevation', **kwargs):
     elev = ds[elev_var]
     
     if groupby is not None:
-        hshade = elev.groupby(groupby).apply(dgeo.hillshade, shortcut=True,
+        # TODO: use shortcut=True
+        # https://github.com/holoviz/datashader/issues/871
+        hshade = elev.groupby(groupby).apply(dgeo.hillshade, shortcut=False,
                                              **kwargs)
     else:
-        hshade = dgeo.hillshade(elev, shortcut=True, **kwargs)
+        hshade = dgeo.hillshade(elev, **kwargs)
     
-    return (hshade
-            .rename(dim_0='y', dim_1='x')
-            .assign_coords(x=ds.x, y=ds.y))
+    return hshade
+
+    # TODO: related to todo above
+    #return (hshade
+    #        .rename(dim_0='y', dim_1='x')
+    #        .assign_coords(x=ds.x, y=ds.y))
